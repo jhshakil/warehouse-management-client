957c0f6a70e3df7965618fa68ac89a5bd50c7c56
Shakil <shakil11271@gmail.com>
2022-04-30 19:50:49 +0600

    set contitional navbar

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,15 +1,30 @@
 import React, { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('')
+    // const auth = getAuth
 
+    const [signInWithEmailAndPassword, user] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+    if (user) {
+        navigate('/')
+    }
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password)
+        signInWithEmailAndPassword(email, password)
+    }
     return (
         <div className='w-3/4 m-auto'>
             <h1 className='text-3xl font-bold text-center mt-8'>Log In</h1>
             <button className='block m-auto p-3 bg-orange-300 rounded-lg mt-8 '>Log in with Google</button>
-            <form className='p-8'>
+            <form onSubmit={handleLogin} className='p-8'>
                 <input ref={emailRef} className='block m-auto bg-gray-300 w-1/2 mt-4 p-2' type="email" placeholder='Enter Your Email' />
                 <input ref={passwordRef} className='block m-auto bg-gray-300 w-1/2 mt-4 p-2' type="password" placeholder='Enter Your Password' />
                 <input className='block m-auto bg-orange-300 w-1/4 mt-4 p-2' type="submit" value="Log in" />
--- src/page/Shared/Header/Header.js
+++ src/page/Shared/Header/Header.js
@@ -1,19 +1,40 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleLogout = () => {
+        signOut(auth)
+    }
     return (
         <div className='grid grid-cols-3 p-5 bg-orange-200'>
             <div>
                 <h1 className='text-5xl ml-16 font-bold'>Car Dealer</h1>
             </div>
-            <div className='flex justify-between text-xl col-span-2 font-bold mr-16'>
-                <Link className='mt-3' to='/'>Home</Link>
-                <Link className='mt-3' to='/manage'>Manage Item</Link>
-                <Link className='mt-3' to='add'>Add Item</Link>
-                <Link className='mt-3' to='myitem'>My Item</Link>
-                <Link className='mt-3' to='login'>Log In</Link>
-                <Link className='mt-3' to='logout'>Log Out</Link>
+            <div className='col-span-2'>
+                {/* <Link className='mt-3' to='/'>Home</Link> */}
+                {/* <Link className='mt-3' to='/manage'>Manage Item</Link>
+                <Link className='mt-3' to='/add'>Add Item</Link>
+                <Link className='mt-3' to='/myitem'>My Item</Link> */}
+                {/* <Link className='mt-3' to='login'>Log In</Link> */}
+                {/* <Link className='mt-3' to='logout'>Log Out</Link> */}
+                {
+                    user
+                        ? <div className='flex justify-end text-xl  font-bold mr-16'>
+                            <Link className='mt-3 mr-8' to='/'>Home</Link>
+                            <Link className='mt-3 mr-8' to='/manage'>Manage Item</Link>
+                            <Link className='mt-3 mr-8' to='/add'>Add Item</Link>
+                            <Link className='mt-3 mr-8' to='/myitem'>My Item</Link>
+                            <Link onClick={handleLogout} className='mt-3 mr-8' to='/login'>Log Out</Link>
+                        </div>
+                        : <div className='flex justify-end text-xl font-bold mr-16'>
+                            <Link className='mt-3 mr-8' to='/'>Home</Link>
+                            <Link className='mt-3 mr-8' to='/login'>Log In</Link>
+                        </div>
+                }
             </div>
         </div>
     );
