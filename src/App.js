5b8761dc3e1fc395dbc45484a21735267ffe85b0
Shakil <shakil11271@gmail.com>
2022-04-30 11:38:21 +0600

    make footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Header from './page/Header/Header';
+import Header from './page/Shared/Header/Header';
 import Home from './page/Home/Home';
+import Footer from './page/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}>Home</Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/page/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-black text-white mt-16 p-16 text-center'>
+            <p>@copyright reserved 2022</p>
+        </div>
+    );
+};
+
+export default Footer;
